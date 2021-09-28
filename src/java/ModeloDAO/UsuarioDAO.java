da4db22ef1c2660ead2c80d54e40c73ab7476627
EstevenMart <emartine0829@gmail.com>
2021-09-27 22:18:42 -0500

    controlador

--- src/java/ModeloDAO/UsuarioDAO.java
+++ src/java/ModeloDAO/UsuarioDAO.java
@@ -6,16 +6,19 @@
 package ModeloDAO;
 
 import Util.Conexion;
-import Util.Crud;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author emart
  */
-public class UsuarioDAO extends Conexion implements Crud {
+public class UsuarioDAO extends Conexion  {
     
     //1. Declarar Atributos y objetos
     private Connection conexion;
@@ -25,23 +28,33 @@ public class UsuarioDAO extends Conexion implements Crud {
     private boolean operacion = false;
     private String sql;
 
-    private String 
+    private String email_usuario = "", nombre_usuario = "", contrasena = "", usuario = "";
+    private int cedula_usuario = 0;
 
-    @Override
-    public boolean agregarRegistro() {
-        
-    }
-
-    @Override
-    public boolean actualizarRegistro() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean eliminarRegistro() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     
+    public boolean iniciarSesion(String usuario, String contrasena) {
+
+        try {
+            conexion = this.obtenerConexion();
+            sql = "select * from usuario where usuario=? and contrasena=?";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, usuario);
+            puente.setString(2, contrasena);
+            mensajero = puente.executeQuery();
+            if (mensajero.next()) {
+                operacion = true;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (SQLException e) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
     
     
 }
